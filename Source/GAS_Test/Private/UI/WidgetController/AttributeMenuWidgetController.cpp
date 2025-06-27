61c9184562941344cee3cd0dbb79b38e171a8b2b
huanglinjie <526532443@qq.com>
2025-06-27 20:52:12 +0800

    # 无

--- Source/GAS_Test/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/GAS_Test/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -3,6 +3,10 @@
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
 
+#include "AuraGameplayTags.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystem/Data/AttributeInfo.h"
+
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
 	
@@ -11,6 +15,11 @@ void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 
 void UAttributeMenuWidgetController::BroadcastInitialValue()
 {
+	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
 
+	ensureMsgf(AttributeInfo,"UAttributeMenuWidgetController.AttributeInfo 未配置");
+
+	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(FAuraGameplayTags::Get().Attributes_Primary_Strength);
+	Info.AttributeValue = AS->GetStrength();
 	
 }
